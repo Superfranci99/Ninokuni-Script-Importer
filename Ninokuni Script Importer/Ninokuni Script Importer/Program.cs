3d34ea42623277fa620965d2661cca519a517dbb
Superfranci99 <superfranci99@gmail.com>
2015-06-12 10:40:46 +0200

    Project created

---
+++ Ninokuni Script Importer/Ninokuni Script Importer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninokuni_Script_Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
