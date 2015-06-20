058860b3cca8769be906da6fa80efaecc82be509
Superfranci99 <superfranci99@gmail.com>
2015-06-20 12:30:29 +0200

    Started Converter

---
+++ Ninokuni Script Importer/Ninokuni Script Importer/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ninokuni_Script_Importer
+{
+    class Converter
+    {
+        public Excel     ExcelData { get; set; }
+        public XDocument XmlDoc    { get; set; }
+        public string    XmlPath   { get; set; }
+
+        public Converter(string excelPath, string xmlPath)
+        {
+            this.ExcelData = new Excel(new FileStream(excelPath, FileMode.Open));      
+            this.XmlDoc = new XDocument();
+            XmlDoc.Declaration = new XDeclaration("1.0", "utf-8", "yes");
+            this.XmlPath = xmlPath;
+        }
+
+        public void StartProcess()
+        {
+            string dataType = this.ExcelData.GetCell(1, 0).ToString().Remove(0, 10);
+            if (dataType != "script")
+                throw new NotImplementedException("DataType conversion still not supported!");
+
+            XElement root = new XElement(dataType);
+        }
+    }
+}
