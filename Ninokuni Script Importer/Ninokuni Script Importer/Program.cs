35b1aeb8ef697dc3b431b226de02585c1abcb9f5
Superfranci99 <superfranci99@gmail.com>
2015-06-30 19:33:51 +0200

    Added font test

--- Ninokuni Script Importer/Ninokuni Script Importer/Program.cs
+++ Ninokuni Script Importer/Ninokuni Script Importer/Program.cs
@@ -4,17 +4,22 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninokuni_Script_Importer.Font;
 
 namespace Ninokuni_Script_Importer
 {
     class Program
     {
-        public static string path = @"D:\Francesco\Desktop\a.xlsx";
+        public static string pathExcel = @"D:\Francesco\Desktop\a.xlsx";
+        public static string pathFont = @"D:\Francesco\Documenti\NDS\Hacking\Laboratory\ninokuni\font\font8.NFTR";
 
         static void Main(string[] args)
         {
-            Converter converter = new Converter(path, @"D:\Francesco\Desktop\out.xml");
-            converter.StartProcess();
+            //Converter converter = new Converter(pathExcel, @"D:\Francesco\Desktop\out.xml");
+            //converter.StartProcess();
+            NFTR font = new NFTR();
+            Stream fs = new FileStream(pathFont, FileMode.Open);
+            font.Read(fs);
         }
     }
 }
