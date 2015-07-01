b96a443c2fdb59bb91f4aec54a85512795975d85
Superfranci99 <superfranci99@gmail.com>
2015-07-01 19:17:09 +0200

    More data read in CGLP

--- Ninokuni Script Importer/Ninokuni Script Importer/Font/CGLP.cs
+++ Ninokuni Script Importer/Ninokuni Script Importer/Font/CGLP.cs
@@ -11,18 +11,33 @@ namespace Ninokuni_Script_Importer.Font
     {
         public override string Name { get { return "CGLP"; } }
 
-        public byte[] MagicID   { get; set; }
-        public uint   BlockSize { get; set; }
+        public byte[] MagicID     { get; set; }
+        public uint   BlockSize   { get; set; }
+        public byte   BoxWidth    { get; set; }
+        public byte   BoxHeight   { get; set; }
+        public ushort BoxByteSize { get; set; }
+        public byte   GlyphHeight { get; set; }
+        public byte   GlyphWidth  { get; set; }
+        public byte   Depth       { get; set; }
+        public byte   Rotation    { get; set; }
 
         public override void Read(Stream stream)
         {
             BinaryReader br = new BinaryReader(stream);
+            long pos = br.BaseStream.Position;
 
-            this.MagicID   = br.ReadBytes(4);
-            this.BlockSize = br.ReadUInt32();
+            this.MagicID     = br.ReadBytes(4);
+            this.BlockSize   = br.ReadUInt32();
+            this.BoxWidth    = br.ReadByte();
+            this.BoxHeight   = br.ReadByte();
+            this.BoxByteSize = br.ReadUInt16();
+            this.GlyphHeight = br.ReadByte();
+            this.GlyphWidth  = br.ReadByte();
+            this.Depth       = br.ReadByte();
+            this.Rotation    = br.ReadByte();
 
-            // this block contains character glyph, this tool doesn't need to read this data...
-            br.BaseStream.Seek(this.BlockSize - 8, SeekOrigin.Current);
+            // skip tile data
+            br.BaseStream.Seek(pos + this.BlockSize, SeekOrigin.Begin);
         }
     }
 }
